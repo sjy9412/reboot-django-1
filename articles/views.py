27aeceea3b8ecec5f4781556da0bdb1ad2776e92
Ssong10 <leesy1403@naver.com>
2019-09-09 15:39:57 +0900

    190909 | reboot-django

---
+++ articles/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-id')
+    context = {
+        'articles' : articles
+    }
+    return render(request,'articles/index.html',context)
+
+def new(request):
+    return render(request,'articles/new.html')
+
+def create(request):
+    article = Article()
+    article.title = request.GET.get('title')
+    article.content = request.GET.get('content')
+    article.created_at = request.GET.get('created_at')
+    article.updated_at = request.GET.get('updated_at')
+    article.save()
+    return redirect(f'/articles/{article.pk}')
+
+def detail(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article' : article
+    }
+    return render(request, 'articles/detail.html',context)
+
+def delete(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('/articles/')
+
+def edit(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article':article
+    }
+    return render(request,'articles/edit.html',context)
+
+def update(request,article_pk):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article.objects.get(pk=article_pk)
+    article.title = title
+    article.content = content
+    article.save()
+    return redirect(f'/articles/{article.pk}')
