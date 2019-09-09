2edd52ab6d81fa7239e79ddef9c549e64fd12879
sjy9412 <sjy9412@gmail.com>
2019-09-09 16:00:02 +0900

    띄어쓰기

--- articles/urls.py
+++ articles/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('new/',views.new),
-    path('create/',views.create),
-    path('<int:article_pk>',views.detail),
-    path('<int:article_pk>/delete/',views.delete),
-    path('<int:article_pk>/edit/',views.edit),
-    path('<int:article_pk>/update/',views.update)
+    path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+    path('<int:article_pk>', views.detail),
+    path('<int:article_pk>/delete/', views.delete),
+    path('<int:article_pk>/edit/', views.edit),
+    path('<int:article_pk>/update/', views.update)
 ]
--- articles/views.py
+++ articles/views.py
@@ -7,10 +7,10 @@ def index(request):
     context = {
         'articles' : articles
     }
-    return render(request,'articles/index.html',context)
+    return render(request, 'articles/index.html',context)
 
 def new(request):
-    return render(request,'articles/new.html')
+    return render(request, 'articles/new.html')
 
 def create(request):
     article = Article()
@@ -21,26 +21,26 @@ def create(request):
     article.save()
     return redirect(f'/articles/{article.pk}')
 
-def detail(request,article_pk):
+def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     context = {
-        'article' : article
+        'article': article
     }
-    return render(request, 'articles/detail.html',context)
+    return render(request, 'articles/detail.html', context)
 
-def delete(request,article_pk):
+def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     article.delete()
     return redirect('/articles/')
 
-def edit(request,article_pk):
+def edit(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     context = {
-        'article':article
+        'article': article
     }
-    return render(request,'articles/edit.html',context)
+    return render(request, 'articles/edit.html', context)
 
-def update(request,article_pk):
+def update(request, article_pk):
     title = request.GET.get('title')
     content = request.GET.get('content')
     article = Article.objects.get(pk=article_pk)
